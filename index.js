018a4b55593ff9e54191e383092f0938926fde8d
Aman Vishnani <aman62@gmail.com>
2016-09-21 13:29:37 +0000

    Added Logic with PIR

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ var PIRpin,PIRvalue,PIR;
 
 var arr = [];
 
+function myVal(x) {
+    if(x==='ON')
+    {
+        return 0;
+    }else if(x === 'OFF')
+    {
+        return 1;
+    }else if(x ==='PIR' && PIRvalue)
+    {
+        return PIRvalue;
+    }else
+    {
+        return 0;
+    }
+}
+
+
 socket.on('connect',function(){
     console.log('Conected');
 });
@@ -32,16 +49,53 @@ socket.on('Success',function(data) {
     }
     for(var i=0;i<data.data.switches.length;i++)
     {
-        arr[i] = new Gpio(data.data.switches[i].GPIO,'out');
+        arr[i].link = new Gpio(data.data.switches[i].GPIO,'out');
+        arr[i].GPIO = data.data.switches[i].GPIO;
+        arr[i].status = data.data.switches[i].status;
+        arr[i].link.writeSync(myVal(arr[i].status));
         console.log("Set "+data.data.switches[i].GPIO+" to Out");
     }
-})
+    socket.emit('Request',APIKey);
+});
+
+socket.on('Update',function(data) {
+    if(data.PIR && PIRpin != data.PIR)
+    {
+        for(var i = 0; i<arr.length;i++)
+        {
+            if(arr[i].GPIO === data.pin)
+            {
+                arr[i].link.unexport();
+                arr.splice(i,1);
+            }
+        }
+        PIRpin = data.PIR;
+        PIR = new Gpio(PIRpin,'in');
+        PIR.watch(function(err,value)
+        {
+            if(err) throw err;
+            PIRvalue = value;
+        });    
+    }
+    for(var i=0;i<data.data.switches.length;i++)
+    {
+        arr[i].link.writeSync(myVal(arr[i].status));
+        console.log("Set "+data.data.switches[i].GPIO+" to "+arr[i].status);
+    }
+    socket.emit('Request',APIKey);
+    return;
+});
+
 
 process.on('SIGINT',function()
 {
+    if(PIR)
+    {
+        PIR.unexport();
+    }
     for(var i=0;i<arr.length;i++)
     {
-        arr[i].unexport();
+        arr[i].link.unexport();
         console.log("Unexport "+i);
     }
     process.exit();
